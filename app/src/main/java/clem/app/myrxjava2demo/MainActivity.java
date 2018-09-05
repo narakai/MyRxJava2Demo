36f47360f188881ed7c2d588dd7f059b338b5f18
laileon <leon@igola.com>
2018-09-05 23:23:09 +0800

    concatmap vs flatmap

--- app/src/main/java/clem/app/myrxjava2demo/MainActivity.java
+++ app/src/main/java/clem/app/myrxjava2demo/MainActivity.java
@@ -4,14 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 //https://www.jianshu.com/p/b39afa92807e
 
@@ -31,17 +39,83 @@ public class MainActivity extends AppCompatActivity {
 //        rxDemo_create();
 //        rxDemo_map();
 //        rxDemo_zip();
-        rxDemo_concat();
+//        rxDemo_concat();
+//        rxDemo_faltmap();
+        rxDemo_concatmap();
     }
 
-//    发射器 B 把自己的三个孩子送给了发射器 A，让他们组合成了一个新的发射器，非常懂事的孩子，有条不紊的排序接收。
+//    concatMap 与 FlatMap 的唯一区别就是 concatMap 保证了顺序，所以，我们就直接把 flatMap 替换为 concatMap 验证吧。
+    private void rxDemo_concatmap() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+            }
+        }).concatMap(new Function<Integer, ObservableSource<String>>() {
+            @Override
+            public ObservableSource<String> apply(Integer integer) throws Exception {
+                List<String> list = new ArrayList<>();
+                //重复三次
+                for (int i = 0; i < 3; i++) {
+                    list.add("I am value " + integer);
+                }
+                int delayTime = (int) (1 + Math.random() * 10);
+                return Observable.fromIterable(list).delay(delayTime, TimeUnit.MILLISECONDS);
+            }
+        }).subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        Log.e(TAG, "flatMap : accept : " + s + "\n");
+                        mRxOperatorsText.append("flatMap : accept : " + s + "\n");
+                    }
+                });
+    }
+
+    //    FlatMap 是一个很有趣的东西，我坚信你在实际开发中会经常用到。它可以把一个发射器 Observable 通过某种方法转换为多个 Observables，
+//    然后再把这些分散的 Observables装进一个单一的发射器 Observable。
+//    但有个需要注意的是，flatMap 并不能保证事件的顺序，如果需要保证，需要用到我们下面要讲的 ConcatMap。
+    private void rxDemo_faltmap() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+            }
+        }).flatMap(new Function<Integer, ObservableSource<String>>() {
+            @Override
+            public ObservableSource<String> apply(Integer integer) throws Exception {
+                List<String> list = new ArrayList<>();
+                //重复三次
+                for (int i = 0; i < 3; i++) {
+                    list.add("I am value " + integer);
+                }
+                int delayTime = (int) (1 + Math.random() * 10);
+                return Observable.fromIterable(list).delay(delayTime, TimeUnit.MILLISECONDS);
+            }
+        }).subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        Log.e(TAG, "flatMap : accept : " + s + "\n");
+                        mRxOperatorsText.append("flatMap : accept : " + s + "\n");
+                    }
+                });
+    }
+
+    //    发射器 B 把自己的三个孩子送给了发射器 A，让他们组合成了一个新的发射器，非常懂事的孩子，有条不紊的排序接收。
     private void rxDemo_concat() {
-        Observable.concat(Observable.just(1,2,3), Observable.just(4,5,6))
+        Observable.concat(Observable.just(1, 2, 3), Observable.just(4, 5, 6))
                 .subscribe(new Consumer<Integer>() {
                     @Override
                     public void accept(Integer integer) throws Exception {
                         mRxOperatorsText.append("concat : ").append(integer).append("\n");
-                        Log.e(TAG, "concat : "+ integer + "\n" );
+                        Log.e(TAG, "concat : " + integer + "\n");
                     }
                 });
     }
