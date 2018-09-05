7d147669cc202f15c10bdb1a3a0826759e85da91
laileon <leon@igola.com>
2018-09-05 22:50:10 +0800

    rxjava2

--- app/src/main/java/clem/app/myrxjava2demo/MainActivity.java
+++ app/src/main/java/clem/app/myrxjava2demo/MainActivity.java
@@ -9,6 +9,11 @@ import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+
+//https://www.jianshu.com/p/b39afa92807e
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -23,6 +28,111 @@ public class MainActivity extends AppCompatActivity {
 
     private void rxjava2Demos() {
         mRxOperatorsText = new StringBuilder();
+//        rxDemo_create();
+//        rxDemo_map();
+//        rxDemo_zip();
+        rxDemo_concat();
+    }
+
+//    发射器 B 把自己的三个孩子送给了发射器 A，让他们组合成了一个新的发射器，非常懂事的孩子，有条不紊的排序接收。
+    private void rxDemo_concat() {
+        Observable.concat(Observable.just(1,2,3), Observable.just(4,5,6))
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        mRxOperatorsText.append("concat : ").append(integer).append("\n");
+                        Log.e(TAG, "concat : "+ integer + "\n" );
+                    }
+                });
+    }
+
+    //    专用于合并事件，该合并不是连接（连接操作符后面会说），而是两两配对，
+//     也就意味着，最终配对出的 Observable 发射事件数目只和少的那个相同。
+    private void rxDemo_zip() {
+        Observable.zip(getStringObservable(), getIntegerObservable(), new BiFunction<String, Integer, String>() {
+            @Override
+            public String apply(String s, Integer integer) throws Exception {
+                return s + integer;
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                mRxOperatorsText.append("zip : accept : ").append(s).append("\n");
+                Log.e(TAG, "zip : accept : " + s + "\n");
+            }
+        });
+    }
+
+    private Observable<String> getStringObservable() {
+        return Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> e) throws Exception {
+                if (!e.isDisposed()) {
+                    e.onNext("A");
+                    mRxOperatorsText.append("String emit : A \n");
+                    Log.e(TAG, "String emit : A \n");
+                    e.onNext("B");
+                    mRxOperatorsText.append("String emit : B \n");
+                    Log.e(TAG, "String emit : B \n");
+                    e.onNext("C");
+                    mRxOperatorsText.append("String emit : C \n");
+                    Log.e(TAG, "String emit : C \n");
+                }
+            }
+        });
+    }
+
+    private Observable<Integer> getIntegerObservable() {
+        return Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                if (!e.isDisposed()) {
+                    e.onNext(1);
+                    mRxOperatorsText.append("Integer emit : 1 \n");
+                    Log.e(TAG, "Integer emit : 1 \n");
+                    e.onNext(2);
+                    mRxOperatorsText.append("Integer emit : 2 \n");
+                    Log.e(TAG, "Integer emit : 2 \n");
+                    e.onNext(3);
+                    mRxOperatorsText.append("Integer emit : 3 \n");
+                    Log.e(TAG, "Integer emit : 3 \n");
+                    e.onNext(4);
+                    mRxOperatorsText.append("Integer emit : 4 \n");
+                    Log.e(TAG, "Integer emit : 4 \n");
+                    e.onNext(5);
+                    mRxOperatorsText.append("Integer emit : 5 \n");
+                    Log.e(TAG, "Integer emit : 5 \n");
+                }
+            }
+        });
+    }
+
+    //    将一个 Observable 通过某种函数关系，转换为另一种 Observable
+    private void rxDemo_map() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(0);
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+            }
+        }).map(new Function<Integer, String>() {
+            @Override
+            public String apply(Integer integer) throws Exception {
+                return "This is result " + integer;
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                mRxOperatorsText.append("accept : ").append(s).append("\n");
+                Log.e(TAG, "accept : " + s + "\n");
+            }
+        });
+    }
+
+    //    用于产生一个 Obserable 被观察者对象
+    private void rxDemo_create() {
         Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(ObservableEmitter<Integer> e) throws Exception {
