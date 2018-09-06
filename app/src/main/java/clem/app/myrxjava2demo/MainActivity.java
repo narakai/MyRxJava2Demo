18dca3706fb2c24354a586e173172145ef3ecd8e
laileon <leon@igola.com>
2018-09-06 15:11:02 +0800

    rxjava2 basic usage done

--- app/src/main/java/clem/app/myrxjava2demo/MainActivity.java
+++ app/src/main/java/clem/app/myrxjava2demo/MainActivity.java
@@ -134,19 +134,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    //    merge 顾名思义，熟悉版本控制工具的你一定不会不知道 merge 命令，而在 Rx 操作符中，merge 的作用是把多个 Observable 结合起来，
-//    接受可变参数，也支持迭代器集合。注意它和 concat 的区别在于，不用等到 发射器 A 发送完所有的事件再进行发射器 B 的发送
-    private void rxDemo_merge() {
-        Observable.merge(Observable.just(1, 2), Observable.just(3, 4, 5))
-                .subscribe(new Consumer<Integer>() {
-                    @Override
-                    public void accept(Integer integer) throws Exception {
-                        mRxOperatorsText.append("merge :" + integer + "\n");
-                        Log.e(TAG, "accept: merge :" + integer + "\n");
-                    }
-                });
-    }
-
     //    取出可观察到的最后一个值，或者是满足某些条件的最后一项
     private void rxDemo_last() {
         Observable.just(1, 2, 3)
@@ -471,6 +458,19 @@ public class MainActivity extends AppCompatActivity {
                     public void accept(String s) throws Exception {
                         Log.e(TAG, "flatMap : accept : " + s + "\n");
                         mRxOperatorsText.append("flatMap : accept : " + s + "\n");
+                    }
+                });
+    }
+
+    //    merge 顾名思义，熟悉版本控制工具的你一定不会不知道 merge 命令，而在 Rx 操作符中，merge 的作用是把多个 Observable 结合起来，
+//    接受可变参数，也支持迭代器集合。注意它和 concat 的区别在于，不用等到 发射器 A 发送完所有的事件再进行发射器 B 的发送
+    private void rxDemo_merge() {
+        Observable.merge(Observable.just(1, 2), Observable.just(3, 4, 5))
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        mRxOperatorsText.append("merge :" + integer + "\n");
+                        Log.e(TAG, "accept: merge :" + integer + "\n");
                     }
                 });
     }
