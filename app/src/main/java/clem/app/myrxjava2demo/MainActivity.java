0bf8075400ccc225b6895b00e00f3931128569ec
laileon <leon@igola.com>
2018-09-06 15:19:28 +0800

    rxjava2 basic usage done

--- app/src/main/java/clem/app/myrxjava2demo/MainActivity.java
+++ app/src/main/java/clem/app/myrxjava2demo/MainActivity.java
@@ -50,8 +50,8 @@ public class MainActivity extends AppCompatActivity {
 //        rxDemo_create();
 //        rxDemo_map();
 //        rxDemo_zip();
-//        rxDemo_concat();
-//        rxDemo_merge();
+        rxDemo_concat();
+        rxDemo_merge();
 //        rxDemo_flatMap();
 //        rxDemo_concatMap();
 //        rxDemo_distinct();
@@ -462,10 +462,11 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
+    //为了演示和concat的差异，一个Observable发射在新线程
     //    merge 顾名思义，熟悉版本控制工具的你一定不会不知道 merge 命令，而在 Rx 操作符中，merge 的作用是把多个 Observable 结合起来，
 //    接受可变参数，也支持迭代器集合。注意它和 concat 的区别在于，不用等到 发射器 A 发送完所有的事件再进行发射器 B 的发送
     private void rxDemo_merge() {
-        Observable.merge(Observable.just(1, 2), Observable.just(3, 4, 5))
+        Observable.merge(Observable.just(1, 2, 3).subscribeOn(Schedulers.io()), Observable.just(4, 5, 6))
                 .subscribe(new Consumer<Integer>() {
                     @Override
                     public void accept(Integer integer) throws Exception {
@@ -477,7 +478,7 @@ public class MainActivity extends AppCompatActivity {
 
     //    发射器 B 把自己的三个孩子送给了发射器 A，让他们组合成了一个新的发射器，非常懂事的孩子，有条不紊的排序接收。
     private void rxDemo_concat() {
-        Observable.concat(Observable.just(1, 2, 3), Observable.just(4, 5, 6))
+        Observable.concat(Observable.just(1, 2, 3).subscribeOn(Schedulers.io()), Observable.just(4, 5, 6))
                 .subscribe(new Consumer<Integer>() {
                     @Override
                     public void accept(Integer integer) throws Exception {
